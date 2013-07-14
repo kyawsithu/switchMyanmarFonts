e40db50136fe04720998f36d23663359b884dd12
kyawsithu <kyawsithu@outlook.com>
2013-07-14 21:55:11 +0630

    Some major changes are made. MyanamrFontTagger is added.

--- contentscript.js
+++ contentscript.js
@@ -4,123 +4,88 @@
  * LICENSE file.
  */
 
-var regexZawgyi = /Zawgyi-One/;
-var regexMyanmar3 = /Myanmar3/;
+// ==UserScript==
+// @name           MyanmarFontTagger
+// @namespace      thantthetkz.mmfonttagger
+// @description    Fill the gap between Zawgyi and Unicode
+// @include        http://*
+// @include        https://*
+// @include        file://*
+// @version        2.0.2
+// @uso:version    2.0.2
+// @copyright  2013, ttkz
+// ==/UserScript==
 
-var fontFamilyName = getFontForElement(document.body);
+(function() {
+    var MutationObserver = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
+    var list = document.querySelector('body');
 
-/*var zawgyiChars = "ကခဒေငပး";
-var zawgyiCharsArray = zawgyiChars.split("");
-
-var myanmar3Chars = "ကခငလေယပ";
-var myanmar3CharsArray = myanmar3Chars.split("");*/
-
-var zawgyiChar = "ၾ";
-var myanmar3Char = "ြ";
-
-/**
- * Check the font name
- * If font name is 'Zawgyi-One' or 'Myanmar3'
- * Auto switch the font name
- */
-if (regexZawgyi.test(fontFamilyName) || findByChar(zawgyiChar) == true) {
-  changeFont('Zawgyi-One');
-  chrome.extension.sendRequest({}, function(response) {});
-} else if (regexMyanmar3.test(fontFamilyName) || findByChar(myanmar3Char) == true){
-  changeFont('Myanmar3');
-  chrome.extension.sendRequest({}, function(response) {});
-}
-
-/**
- * Find the fonts used in page by using char array
- * @argument {CharArray} char array
- * @returns {boolean} boolean return if char is found
- */
-function findByChars(arr) {
-	for (i = 0; i < arr.length; i++) {
-		var re = new RegExp(arr[i], "g");
-		if (re.test(document.body.innerText)) {
-			return true;
-		}
-	}
-}
-
-function findByChar(regxChar) {
-	var re = new RegExp(regxChar, "g");
-	if (re.test(document.body.innerText)) {
-		return true;
-	}else{
-		return false;
-	}
-}
-
-/**
- * Get the font used for a given element
- * @argument {HTMLElement} the element to check font for
- * @returns {string} The name of the used font or null if font could not be detected
- */
-function getFontForElement(ele) {
-    if (ele.currentStyle) { // sort of, but not really, works in IE
-        return ele.currentStyle["fontFamily"];
-    } else if (document.defaultView) { // works in Opera and FF
-        return document.defaultView.getComputedStyle(ele,null).getPropertyValue("font-family");
-    } else {
-        return null;
-    }
-}
-
-/**
- * Switch the font
- * @argument {fontname} name of the font to switch
- * @returns {void} 
- */
-function changeFont(fontname) {
-    fontfamily = fontname;
-	if(fontfamily == 'Myanmar3')
-	{
-		addCssStyleSheet('http://mmwebfonts.comquas.com/fonts/?font=myanmar3');
-	}
-    document.getElementsByTagName('body')[0].style.fontFamily = fontfamily;
-    var tag = ['body', 'p', 'li', 'span', 'textarea', 'input', 'div', 'a', 'td', 'h1', 'h2', 'h3'];
-    var p;
-    for (j = 0; j < tag.length; j++) {
-        if (document.getElementsByTagName(tag[j]) != null) {
-            p = document.getElementsByTagName(tag[j]);
-            for (i = 0; i < p.length; i++) {
-                if (p[i].style != undefined) {
-                    p[i].style.fontFamily = fontfamily;
-                }
-            }
-        }
-    }
-    var iframe = document.getElementsByTagName('iframe');
-    for (k = 0; k < iframe.length; k++) {
-        doc = iframe[k].contentDocument;
-        for (j = 0; j < tag.length; j++) {
-            if (document.getElementsByTagName(tag[j]) != null) {
-                p = doc.getElementsByTagName(tag[j]);
-                for (i = 0; i < p.length; i++) {
-                    if (p[i].style != undefined) {
-                        p[i].style.fontFamily = fontfamily;
+    var observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            if (mutation.type == 'childList') {
+                for (var i = 0; i < mutation.addedNodes.length; i++) {
+                    var node = mutation.addedNodes[i];
+                    if (node.nodeType == Node.TEXT_NODE) {} else {
+                        // console.log(node, node.textContent);
+                        tagNode(node);
                     }
                 }
+            } else if (mutation.type == 'characterData') {
+                tagNode(mutation.target);
+            }
+                });
+    });
+    
+    observer.observe(list, {
+        childList: true,
+        attributes: false,
+        characterData: true,
+        subtree: true
+    });
+    
+    /*
+    The following detection pattern is based on the script by Ko Ravi
+    Font Busters - http://userscripts.org/scripts/show/42941
+    */
+    var regexMM = new RegExp("[\u1000-\u109f\uaa60-\uaa7f]+");
+    var regexUni = new RegExp("[ဃငဆဇဈဉညဋဌဍဎဏဒဓနဘရဝဟဠအ]်|ျ[က-အ]ါ|ျ[ါ-း]|[^\1031]စ်|\u103e|\u103f|\u1031[^\u1000-\u1021\u103b\u1040\u106a\u106b\u107e-\u1084\u108f\u1090]|\u1031$|\u1031[က-အ]\u1032|\u1025\u102f|\u103c\u103d[\u1000-\u1001]|ည်း|ျင်း|င်|န်း|ျာ|[ာ်ါ]တ်|ြို");
+    var regexZG = new RegExp("\s\u1031| ေ[က-အ]်|[က-အ]း");
+    var timerID = undefined;
+    var mmFonts = new RegExp("Zawgyi-One|Masterpiece Uni Sans|Myanmar3|Yunghkio|Parabaik|WinUni Innwa|Win Uni Innwa|Padauk|MyMyanmar|Panglong|Ayar|TharLon|Myanmar Sangam");
+    var unicodeFonts = new RegExp("Masterpiece Uni Sans|Myanmar3|Yunghkio|Parabaik|WinUni Innwa|Win Uni Innwa|Padauk|MyMyanmar|Panglong|TharLon|Myanmar Sangam");
+    var useUnicodeFont = "Masterpiece Uni Sans, TharLon, Myanmar3, Yunghkio, Padauk, Parabaik, WinUni Innwa, Win Uni Innwa, MyMyanmar Unicode, Panglong, Myanmar Sangam MN, Myanmar MN";
+    
+    var facebook_wordbreaking_classes = ['tlActorText', 'messageBody', 'commentBody', 'uiAttachmentTitle', 'uiAttachmentDesc', 'ministoryInlineMessage', 'msg'];
+    var unicode_word_breaker = /[\u1000-\u102A\u1040-\u1049\u103F](?!\u103A)/g;
+    var classNameUnicode = "__ft_unicode";
+    var classNameZawgyi = "__ft_zawgyi";
+    
+    var tagNode = function(node) {
+        if (node.className && node.className.indexOf('_tt_t_') !== -1) {
+            return;
+        }
+        if (node.nodeType == Node.TEXT_NODE) {
+            var text = node.textContent;
+            if (!regexMM.test(text)) {
+                return;
+            }
+            if (text) {
+                var prNode = node.parentNode;
+                text = prNode.textContent;
+                if (regexUni.test(text) && !regexZG.test(text)) {
+                    if (prNode.style) prNode.style.fontFamily = useUnicodeFont;
+                } else {
+                    if (prNode.style) prNode.style.fontFamily = "Zawgyi-One";
+                }
+                prNode.className += ' _tt_t_';
+            }
+        } else {
+            for (var i = 0; i < node.childNodes.length; i++) {
+                var child = node.childNodes[i];
+                tagNode(child);
             }
         }
     }
-}
-
-/**
- * Add css style sheet to head tag
- * @argument {cssFile} css file link to add
- * @returns {void} 
- */
-function addCssStyleSheet(cssFile)
-{
     
-    var js = document.createElement("link");
-    js.setAttribute("type","text/css");
-	js.setAttribute("rel","stylesheet");
-    js.setAttribute("href",cssFile)
-    document.getElementsByTagName("head")[0].appendChild(js)
-    return false;
-}
+    if (document && document.body) tagNode(document.body);
+})();
